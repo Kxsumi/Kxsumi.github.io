e71a71586ee2bdf37809f43e5e81da8e12fc20fa
Atlas <96434998+Kxsumi@users.noreply.github.com>
2024-05-02 13:54:22 -0400

    Delete src directory

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Component, ComponentProps } from 'solid-js'
-
-export * from './buttons'
-export * from './Page'
-export { default as NavDock } from './NavDock'
-export { default as ProjectCard } from './ProjectCard'
-export { default as Touchable } from './Touchable'
-
-export type IconType = Component<ComponentProps<'svg'>>
--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@solidjs/start/env" />
-
-declare const __APP_COMMIT: string
-declare const __APP_DEPLOY_CONTEXT: string
-declare const __APP_BRANCH: string
-declare const __APP_INTEGRITY: 'clean' | 'dirty' | 'unknown'
-declare const __APP_INTEGRITY_DIRTY_FILES: string[]
